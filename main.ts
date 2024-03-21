d27683e0c312ab2ac4ddc6a8ad556cc47b0fe787
terminal <terminal>
2024-03-21 19:23:29 +0000

    Added a console log

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 import { FetchLatestArticle } from "./lib/workflows/fetchFromWeb3";
 import { algoliaClient, pushToAlgolia } from "./lib/workflows/pushToAlgolia";
+const INDEX = "dev_articles";
+console.log("Using index: ", INDEX, "for Algolia. You can change this in the .env file.");
 
 (async()=>{
     const client = algoliaClient;
     const indexName = 
         process.env.ALGOLIA_INDEX_NAME == undefined 
-        ? "daily-pepe-test-index-article" 
+        ? "dev_articles" 
         : process.env.ALGOLIA_INDEX_NAME;
     await FetchLatestArticle().then((res) => {
         return pushToAlgolia(client, indexName, [res]);
