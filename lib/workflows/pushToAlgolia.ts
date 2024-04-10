b4bb945ba64f2878487e3c482aacc4d5fe69a28f
sblevins <stephenblevins@comcast.net>
2024-04-11 08:42:28 +1100

    serializable articles

--- lib/workflows/pushToAlgolia.ts
+++ lib/workflows/pushToAlgolia.ts
@@ -1,6 +1,18 @@
 import algoliasearch, { SearchClient } from 'algoliasearch';
 import { Article } from '../../types/apiTypes';
 
+declare type SerializableArticle = {
+  articleDate: string //format DD-MM-YYYY
+  articleTitle: string
+  articleImageURL: URL | string
+  articleText: string
+  articleAuthor: string
+  mintStart: string //earliest time this article can be minted in unix time (seconds)
+  mintEnd: string //deadline to mint this article in unix time (seconds)
+  tags: string[]
+  articleId: string
+}
+
 // Clean this up later
 export const algoliaClient = algoliasearch(
     process.env.ALGOLIA_APP_ID == undefined ? "" : process.env.ALGOLIA_APP_ID,
@@ -13,5 +25,22 @@ export async function pushToAlgolia(
   records: Article[]
 ) {
   const index = client.initIndex(indexName);
-  await index.saveObjects(records);
+  const recordsWithIds = records.map(record => {
+    const serializableRecord:SerializableArticle = {
+      articleDate: record.articleDate,
+      articleTitle: record.articleTitle,
+      articleImageURL: record.articleImageURL,
+      articleText: record.articleText,
+      articleAuthor: record.articleAuthor,
+      mintStart: record.mintStart.toString(),
+      mintEnd: record.mintEnd.toString(),
+      tags: record.tags,
+      articleId: record.articleId.toString()
+    }
+    return {
+      objectID: record.articleId.toString(),
+      article: serializableRecord,
+    }
+  })
+  await index.saveObjects(recordsWithIds);
 }
